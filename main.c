512fa81d8ee2b3ad57f35c5ebdd233a49c04df00
STAR123667 <58503610+STAR123667@users.noreply.github.com>
2023-05-15 17:41:59 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,305 +1,86 @@
 #include "main.h"
-uint32_t KeyTime;  //保存KEY按下时间
-uint16_t KeyCount; //记录KEY按下次数
-uint16_t VBat_Start; //电池状态次数
-uint16_t TimeCount3s;//时间计数
-extern bool Led_Flag;      //LED标志//
-extern bool Phase_Flag;    //相序标志//
-extern uint8_t Phase_Start;//相序状态//
-extern bool PhaseLine_Flag;
-struct Flag
-{ 
-   uint8_t c:1;
-   uint8_t Power_Run:1;
-}Key_flag;		
-void Get_Phase(void);
-int main()
-{	
-   COMP_Cmd(COMP_Selection_COMP1, DISABLE); 
-   COMP_Cmd(COMP_Selection_COMP2, DISABLE);		
-   PWR_PVDCmd(DISABLE);	
-   PWR_BackupAccessCmd(DISABLE);	
-   RTC->CR = RTC->CR & 0xffffff7f;	
-   RCC_LSICmd(DISABLE);
-   while(RCC_GetFlagStatus(RCC_FLAG_LSIRDY) !=RESET);	
-	
-   Power_En_Init();       //电源引脚初始化
-   Buzzer_Init();         //蜂鸣器引脚初始化
-   Vbat_Init();           //电池采样引脚初始化
-   Time14_Init();   	  //定时器14 500US刷新A B 引脚初始化
-   Time17_Init();	      //定时器17 参考信号引脚初始化
-   Time6_Init();          //定时器6  计时1MS
-   Time3_Init();	      //定时器3  计时100US 
-   Sensor_Init();         //运放输入引脚初始化 	
-	
-   VBat_Start = ADC_Get_VBat();	        //获取电池电压2000次，使开机电池处于稳定状态。	
-   // IWDG_Config(IWDG_Prescaler_64,909);  //看门狗配置3S  	
-   
-   while(1)
-   {   
-           // 喂狗
-             // IWDG_Feed();
+/******************************************************************************
+;  *----------------------摘要描述---------------------------------
+;  *   注:      本例程测试EXTI外部事件唤醒SLEEP
+;  *  	 PA4 EXTI外部事件输入, PA10 输出    
+;  *     PA4 上升沿触发事件,唤醒SLEEP,翻转对应IO一次
+******************************************************************************/
+u32 KeyTime=0;
+bool keyflag;
+#define KEY_IN __GPIO_INPUT_PIN_GET(GPIOA, GPIO_PIN_4)
 
-            if(!Phase_Flag)
-			{   
-		      Scan_Key();        //按键输入	  
-			}       
-	        else 
-			{  
-		      Get_Phase();	     
-			}
-	          Get_Battery();     //电池电压获取,低于6V报警
+void cs32_gpio_init(void);
+void cs_eval_led_toggle(void);
+void delay_1ms(uint16_t TimeDelay);
+
+int main(void)
+{	
+   cs32_gpio_init();
+
+   while(1)
+   {
+  	   
+	 if(KEY_IN != RESET)
+    {
+		delay_1ms(10);
+		if(KEY_IN != RESET)
+		{
+	     if(!keyflag)
+		 {
+		   cs_eval_led_toggle();	 
+		 }			 
+	 	  keyflag=1;		
+		}
+    }
+	else 
+	{
+	      keyflag=0;
+		    pmu_deep_sleep_mode_enter(PMU_LDO_LOW_POWER, PMU_DSM_ENTRY_WFE);
+	}
+  
+	   
    }
 }
-void Scan_Key(void)
+void cs32_gpio_init(void)
 {
-	      uint8_t j,i;	
-	  	
-          if( Power_En_In != Bit_SET)	  
-		  {
-            Key_flag.c =0;			
-		  }   
-  
-          if( Power_En_In == Bit_SET && Key_flag.c == 0 )	 
-	      {		  
-            Delay_ms(100);  
-	      if( Power_En_In == Bit_SET && Key_flag.c == 0)	  
-	      {          
-           Key_flag.c =1;
-		  }
-		  }		  
-		  if(Key_flag.c !=0)
-		  { 
-		   Power_En_H;    /* 电源上电 */	
-		   Delay_ms(10); 
-		   KeyTime++;   
-		  }
-          
-		   if(Key_flag.c !=1)
-		  {
-//			IWDG_Feed();  		
-		   if(KeyTime >1 && KeyTime <=100)  
-		  {
-			  
-		  if(!PhaseLine_Flag)
-		  {		
-		    KeyCount = KeyCount+1;
-		    switch (KeyCount)
-            {
-          	case 1:
-			{ 
-			  Power_En_H;    /* 电源上电 */	           			  
-			 if(Key_flag.Power_Run==0)
-			 {		  
-			   for(j=0; j<200; j++)
-               {
-	            TIM_Cmd(TIM15, ENABLE);
-                TIM_CtrlPWMOutputs(TIM15, ENABLE);  
-                Delay_ms(1);
-               }	
-                TIM_Cmd(TIM15, DISABLE);
-                TIM_CtrlPWMOutputs(TIM15, DISABLE); 
-			 
-                Key_flag.Power_Run =1;		   
-                Led_Flag =1;
-                Led_Out_Low();		
-			    Delay_ms(1000);
-			    Led_Out_Close();
-				// IWDG_Feed();  
-			     for(i=1;i<=5;i++)
-			     {
-				   // IWDG_Feed();  
-    			   PowerUpStart1(i);   
-				   Delay_ms(400); 
-                   Get_Battery(); 	
-			     }
-                  TIM_Cmd(TIM6, ENABLE); 
-			  }		     
-			  Led_Flag =1;
-			  Led_Out_Low();			 
-			  KeyTime = 0;	  			         
-              TimeCount3s = 0; 		
-              // TIM_Cmd(TIM6, ENABLE);   			  
-			  TIM_Cmd(TIM14, DISABLE); 			  
-			 } 
-              break;
-          	case 2:
-			{ 
-			  Power_En_H;    /* 电源上电 */	  
-			  Led_Out_Hight(); 
-			  KeyCount =0; 
-			  KeyTime =0;		
-			  TimeCount3s =0;
-			  // TIM_Cmd(TIM6, ENABLE);   
-              TIM_Cmd(TIM14, DISABLE); 
-		    }
-          		break;
-          	default:
-          		break;
-            }
-		 }
-			else 
-			{ 
-			  KeyTime=0; 
-			  KeyCount=0;
-		      return;
-			}
-	   }			
-		   else if(KeyTime > 100) 
-		   {  
-	       Led_Out_Close(); 
-		   TIM_Cmd(TIM6, DISABLE);  
-           TIM_Cmd(TIM14, DISABLE); 		   
-		   KeyTime =0;	  
-		   KeyCount=0; 
-		   Key_flag.Power_Run =0;			  
-		   TimeCount3s =0;	
-		   Power_En_L;
-		   }		   
-           else 
-           {  
-	      			
-            if(TIM_GetFlagStatus(TIM6, TIM_FLAG_Update) == SET)  
-	        {
-			    TIM_ClearFlag(TIM6, TIM_FLAG_Update);
-			    TimeCount3s ++; 
-                if(TimeCount3s == 1000) 		          //1秒除电源指示灯亮，其他不亮
-                { 	
-                Led_Out_Close();						
-                Power_Led_Open;
-				TimeCount3s=0;
-                Time3_Init();
-                TIM_Cmd(TIM6, DISABLE);  
-				TIM_Cmd(TIM14, ENABLE);     //定时器14 使能。A B间隔扫描	   		   
-				Led_Flag=0;  	
-				
-					
-                RCC_HSICmd(ENABLE);
-				while(RCC_GetFlagStatus(RCC_FLAG_HSIRDY) !=SET);
-                /* Enable Prefetch Buffer and set Flash Latency */
-		        FLASH->ACR = FLASH_ACR_PRFTBE | FLASH_ACR_LATENCY;
-		        /* HCLK = SYSCLK */
-		        RCC->CFGR |= (uint32_t)RCC_CFGR_HPRE_DIV1;
-		        /* PCLK = HCLK */
-		        RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE_DIV1;
-		        // PLL configuration = (HSI/2) * 12 = 48 MHz
-		        RCC_PLLConfig(RCC_PLLSource_HSI_Div2, RCC_PLLMul_2); // 8M/2 * 2 = 8M
-		        /* Enable PLL */
-		        RCC->CR |= RCC_CR_PLLON;
-		        /* Wait till PLL is ready */
-		        while ((RCC->CR & RCC_CR_PLLRDY) == 0)
-		        {
-		        }
-		        /* Select PLL as system clock source */
-		        RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK); // PLL 做系统时钟
-		        /* Wait till PLL is used as system clock source */
-		        while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS) != (uint32_t)RCC_CFGR_SWS_PLL)
-		        {
-		        }	
-			  			
-				PWR_EnterSleepMode(PWR_SLEEPEntry_WFI);	   //进入休眠//
-						
-			    RCC_HSICmd(ENABLE);			
-				while(RCC_GetFlagStatus(RCC_FLAG_HSIRDY) !=SET);
-				/* Enable Prefetch Buffer and set Flash Latency */
-		        FLASH->ACR = FLASH_ACR_PRFTBE | FLASH_ACR_LATENCY;
-		        /* HCLK = SYSCLK */
-		        RCC->CFGR |= (uint32_t)RCC_CFGR_HPRE_DIV1;
-		        /* PCLK = HCLK */
-		        RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE_DIV1;
-		        // PLL configuration = (HSI/2) * 12 = 48 MHz
-		        RCC_PLLConfig(RCC_PLLSource_HSI_Div2, RCC_PLLMul_12); // 8M/2 * 12 = 48M
-		        /* Enable PLL */
-		        RCC->CR |= RCC_CR_PLLON;
-		        /* Wait till PLL is ready */
-		        while ((RCC->CR & RCC_CR_PLLRDY) == 0)
-		        {
-		        }
-		        /* Select PLL as system clock source */
-		        RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK); // PLL 做系统时钟
-		        /* Wait till PLL is used as system clock source */
-		        while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS) != (uint32_t)RCC_CFGR_SWS_PLL)
-		        {
-		        }	
-
-	            TIM_Cmd(TIM6, ENABLE);	
-				
-		        }	  
- 			} 	
-            else 
-		    {   
-			    return;
-			}		
+   nvic_config_t nvic_config_struct;
 	
-           }   
-		   
-		}
+  __RCU_DEF_INIT();                            /*初始化外设时钟*/
+  __RCU_AHB_CLK_ENABLE(RCU_AHB_PERI_GPIOA);    /*使能GPIOA时钟*/	
+  __RCU_APB2_CLK_ENABLE(RCU_APB2_PERI_SYSCFG); /*使能SYSCFG时钟*/	
+ 
+  syscfg_exti_line_config(SYSCFG_EXTI_PORT_PA, SYSCFG_EXTI_PIN_4); // Connect EXTI4 Line to PA4 pin	
+  /*配置GPIOA_10 输出模式*/
+  gpio_mode_set(GPIOA, GPIO_PIN_10, GPIO_MODE_OUT_PP(GPIO_SPEED_HIGH));   
+  /*配置GPIOA_10 输出低*/
+  __GPIO_PIN_SET(GPIOA, GPIO_PIN_10);
+  /*配置GPIOA_4 输入模式*/
+  gpio_mode_set(GPIOA, GPIO_PIN_4, GPIO_MODE_IN_FLOAT);  
+	/*配置外部中断 模式*/
+  EXTI->INTEN &= 0xffffffef;	  /*中断线Line4不使能*/ 
+  EXTI->EVTEN |= 0x00000010;	  /*事件线Line4使能*/ 
+  EXTI->RTEN  |= 0x00000010;	  /*上升沿中断使能*/ 
+  EXTI->SWTIEN|= 0x00000010;	  /*使能Line4软件触发中断/事件使能*/ 
 }
-void Get_Battery(void)
-{
-          VBat_Start = ADC_Get_VBat();    //获取电池电压   
-
-		  if(VBat_Start<2000)   
-         { 
-	        Led_Out_Close();
-	        for(;1;) 
-			{ 
-		     // 喂狗
-            // IWDG_Feed();
-
-		     Power_Led_Close;
-			 Delay_ms(150);  
-           	 Power_Led_Open;
-             Delay_ms(150);	 
-				
-			 VBat_Start = ADC_Get_VBat();
-				
-             if(VBat_Start>=2000)            //电池电压正常返回,6.5V以上
-			 {
-			   break;
-			 }
-      		}
-	      }                            //是1 电池电压低于7V报警处理 
-          else 
-          {
-		    return;
-		  }			               //是0 电池电压正常	
-}
-void IWDG_Config(uint8_t prv ,uint16_t rlv)
+void cs_eval_led_toggle(void)
 {	
-	// 使能 预分频寄存器PR和重装载寄存器RLR可写
-	IWDG_WriteAccessCmd(IWDG_WriteAccess_Enable);
-	// 设置预分频器值
-	IWDG_SetPrescaler(prv);
-	// 设置重装载寄存器值
-	IWDG_SetReload(rlv);
-	// 把重装载寄存器的值放到计数器中
-	IWDG_ReloadCounter();
-	// 使能 IWDG
-	IWDG_Enable();	
+	GPIOA->DO ^= GPIO_PIN_10;
 }
-// 喂狗
-void IWDG_Feed(void)
+/**********  软件普通延时  **********/
+/**
+  * @fn void delay_1ms(uint16_t TimeDelay)
+  * @brief  Simple delay 1ms      
+  * @param  None
+  * @return None
+  */
+void delay_1ms(uint16_t TimeDelay)
 {
-	// 把重装载寄存器的值放到计数器中，喂狗，防止IWDG复位
-	// 当计数器的值减到0的时候会产生系统复位
-	IWDG_ReloadCounter();
-}
-void Get_Phase(void)
-{
-      	    IWDG_Feed();
-  		   	if(Phase_Start == 0x01)           /* 正序 */		
-			{			
-					BuzzerOpen_Speen(0x02);   /* 开蜂鸣器 */		
-			}
-			else if(Phase_Start == 0x02)      /* 负序 */		
-			{
-					BuzzerOpen_Speen(0x01);   /* 开蜂鸣器 */
-			}
-			else 
-			{
-				    BuzzerClose_Speen();      /* 关闭蜂鸣器 */
-			}		
+    int i,j;
+    for(i=0;i<TimeDelay*35;i++)
+    {
+        for(j=0;j<38;j++)
+        __NOP();
+    }
 }
 
 
---
+++ main.h
@@ -0,0 +1,14 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+/* Includes ------------------------------------------------------------------*/
+#include "cs32f0xx.h"
+#include "stdbool.h"
+/* Exported types ------------------------------------------------------------*/
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t u8;
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+#endif /* __MAIN_H */
